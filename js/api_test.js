070382e91f35dfc6356980d7a6ab9be629c814ef
Antonín <71491435+antoninvondrovic@users.noreply.github.com>
2022-03-04 14:50:57 +0100

    implemented movie class

--- js/api_test.js
+++ js/api_test.js
@@ -3,6 +3,17 @@ window.onload = async () => {
     const dataList = document.getElementById('adminData');
     const b = await fetch('http://whofucking.farted.net/api/movies').then((response) => response.json());
 
+    class Movie {
+        constructor(id, name, description, ageRating, image, extraInfo) {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.ageRating = ageRating;
+            this.image = image;
+            this.extraInfo = extraInfo;
+        }
+    }
+
     let movies = [];
 
     for (let i = 0; i < b.length; i++) {
