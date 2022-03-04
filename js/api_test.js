13377aad6a2a1bcadf65a6b8aeb35215c2f0bc1e
Antonín <71491435+antoninvondrovic@users.noreply.github.com>
2022-03-04 14:45:12 +0100

    testing api

--- js/api_test.js
+++ js/api_test.js
@@ -1,5 +1,6 @@
 window.onload = async () => {
-    const container = document.getElementById('adminData');
+    const mid = document.getElementById('middle');
+    const dataList = document.getElementById('adminData');
     const b = await fetch('http://whofucking.farted.net/api/movies').then((response) => response.json());
 
     let movies = [];
@@ -10,8 +11,14 @@ window.onload = async () => {
     }
 
     for (let i = 0; i < movies.length; i++) {
+        dataList.remove();
+        const dat = document.createElement('div');
+        dat.classList.add('adminData');
+        dat.id = 'adminData';
+        mid.appendChild(dat);
+
         const name = document.createElement('p');
         name.innerText = movies.name;
-        container.appendChild(name);
+        dataList.appendChild(name);
     }
 };
